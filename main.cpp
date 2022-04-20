8da1d68e780d5dc57c1748127053f1211569c696
jtshiv <shiversjt@gmail.com>
2022-04-19 23:26:56 -0400

    Tested out different data types in cpp. Also worked with some string functions

--- main.cpp
+++ main.cpp
@@ -14,7 +14,22 @@ int main()
     std::cout << "How old were they? ";
     std::cout << "They were probably " << authorAge << " years old.";
 
+    //Testing out different data types
+    char grade = 'A';
+    std::string phrase = "I know what these are";
+    int age = 100; //Integers can't have decimals
+    double gpa = 5.2; //larger than float
+    bool isMale = true; //the true and false variable boolean
 
+    std::cout << "\n" << grade << phrase << age << gpa << isMale;
+
+    //Testing string functions
+    std::cout << "\n\n" << phrase.length();
+    std::cout << "\n" << phrase[0]; //get individual chars
+    phrase[0]='B'; //replacing indiv chars
+    std::cout << "\n" << phrase << "\n";
+    std::cout << phrase.find("know",0) << "\n"; //find string in string from first char
+    std::cout << phrase.substr(8,4); //get substring from char 8 for 4 chars
 
     return 0;
 }
