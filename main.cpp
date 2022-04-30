e0bae34d5c78dd94bfa586c52fc1f3edf0ebdde9
Tyler Shivers <jtshiv@users.noreply.github.com>
2022-04-29 22:22:26 -0400

    Update main.cpp
    Basic calculator for adding two numbers

--- main.cpp
+++ main.cpp
@@ -5,11 +5,13 @@ using namespace std;
 
 int main()
 {
-    string name;
-    cout << "Enter your name: ";
-    getline(cin, name);
-
-    cout << "Hello " << name;
+    int num1, num2;
+    cout << "Welcome to the calculator" << endl;
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter second number: ";
+    cin >> num2;
+    cout << "The sum of those is " << num1 + num2;
 
     //below is to pause console from closing
     std::getchar();
