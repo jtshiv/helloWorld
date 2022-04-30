a1964c14cc535ba320eaf38f459124c70958ab1c
Tyler Shivers <jtshiv@users.noreply.github.com>
2022-04-29 22:19:03 -0400

    Update main.cpp
    Swapped ints to string to ask for name. Had to add include string for the getline

--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
+#include <string>//needed for getline
 
 using namespace std;
 
 int main()
 {
-    int age;
-    cout << "Enter your age: ";
-    cin >> age;
+    string name;
+    cout << "Enter your name: ";
+    getline(cin, name);
 
-    cout << "You are " << age << " years old.";
+    cout << "Hello " << name;
 
     //below is to pause console from closing
     std::getchar();
