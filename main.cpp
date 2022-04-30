2b9f71dc2cf63f3683e0a4ffaf2d7dd16c2403da
Tyler Shivers <jtshiv@users.noreply.github.com>
2022-04-29 22:12:12 -0400

    Update main.cpp
    Removed old
    
    Working with user prompts.
    
    added getchars to bottom to pause console before closing

--- main.cpp
+++ main.cpp
@@ -1,38 +1,17 @@
 #include <iostream>
-#include <cmath> //added to use advanced math functions
 
 using namespace std;
+
 int main()
 {
-    cout << 5 + 7;
-    //They have to start with this
-    cout << "Hello World!!!!!!";
+    int age;
+    cout << "Enter your age: ";
+    cin >> age;
 
-    //Testing out variables
-    string authorName = "Dumbledore";
-    int authorAge;
-    authorAge = 87;
-    cout << "Who wrote this? ";
-    cout << "It was probably " << authorName << ". ";
-    cout << "How old were they? ";
-    cout << "They were probably " << authorAge << " years old.";
+    cout << "You are " << age << " years old.";
 
-    //Testing out different data types
-    char grade = 'A';
-    string phrase = "I know what these are";
-    int age = 100; //Integers can't have decimals
-    double gpa = 5.2; //larger than float
-    bool isMale = true; //the true and false variable boolean
-
-    cout << "\n" << grade << phrase << age << gpa << isMale;
-
-    //Testing string functions
-    cout << "\n\n" << phrase.length();
-    cout << "\n" << phrase[0]; //get individual chars
-    phrase[0]='B'; //replacing indiv chars
-    cout << "\n" << phrase << "\n";
-    cout << phrase.find("know",0) << "\n"; //find string in string from first char
-    cout << phrase.substr(8,4); //get substring from char 8 for 4 chars
-
+    //below is to pause console from closing
+    std::getchar();
+    std::getchar();
     return 0;
 }
